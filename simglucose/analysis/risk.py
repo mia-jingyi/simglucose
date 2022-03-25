d70024cca59bf502693ff2d953c79e458eb60817
Jingyi Yu <jingyi.yu@rwth-aachen.de>
2022-03-25 20:20:08 +0100

    Crop BG at 1 and add magni risk index

--- simglucose/analysis/risk.py
+++ simglucose/analysis/risk.py
@@ -7,7 +7,8 @@ def risk_index(BG, horizon):
     # horizon in samples
     with warnings.catch_warnings():
         warnings.simplefilter('ignore')
-        BG_to_compute = BG[-horizon:]
+        BG_to_compute = np.array(BG[-horizon:])
+        BG_to_compute[BG_to_compute < 1] = 1  # crop at 1
         fBG = 1.509 * (np.log(BG_to_compute)**1.084 - 5.381)
         rl = 10 * fBG[fBG < 0]**2
         rh = 10 * fBG[fBG > 0]**2
@@ -15,3 +16,10 @@ def risk_index(BG, horizon):
         HBGI = np.nan_to_num(np.mean(rh))
         RI = LBGI + HBGI
     return (LBGI, HBGI, RI)
+
+
+def magni_risk_index(BG, **kwargs):
+    bg = max(BG[-1], 1)
+    fBG = 3.5506*(np.log(bg)**.8353-3.7932)
+    risk = 10 * (fBG)**2
+    return risk
