c17b41de1b0e452a6b87780381e4925658dd4629
Jingyi Yu <jingyi.yu@rwth-aachen.de>
2022-03-25 21:30:36 +0100

    Add ManualBBController

--- simglucose/controller/basal_bolus_ctrller.py
+++ simglucose/controller/basal_bolus_ctrller.py
@@ -3,10 +3,12 @@ from .base import Action
 import numpy as np
 import pandas as pd
 import logging
+from collections import namedtuple
 
 logger = logging.getLogger(__name__)
 CONTROL_QUEST = 'simglucose/params/Quest.csv'
 PATIENT_PARA_FILE = 'simglucose/params/vpatient_params.csv'
+ParamTup = namedtuple('ParamTup', ['basal', 'cf', 'cr'])
 
 
 class BBController(Controller):
@@ -78,3 +80,70 @@ class BBController(Controller):
 
     def reset(self):
         pass
+
+
+class ManualBBController(Controller):
+    def __init__(self, target, cr, cf, basal, sample_rate=5, use_cf=True, use_bol=True, cooldown=0,
+                 use_low_lim=False, low_lim=70):
+        super().__init__(self)
+        self.target = target
+        self.orig_cr = self.cr = cr
+        self.orig_cf = self.cf = cf
+        self.orig_basal = self.basal = basal
+        self.sample_rate = sample_rate
+        self.use_cf = use_cf
+        self.use_bol = use_bol
+        self.cooldown = cooldown
+        self.last_cf = np.inf
+        self.use_low_lim = low_lim
+        self.low_lim = low_lim
+
+    def increment(self, cr_incr=0, cf_incr=0, basal_incr=0):
+        self.cr += cr_incr
+        self.cf += cf_incr
+        self.basal += basal_incr
+
+    def policy(self, observation, reward, done, **kwargs):
+        carbs = kwargs.get('carbs')
+        glucose = kwargs.get('glucose')
+        action = self.manual_bb_policy(carbs, glucose)
+        return action
+
+    def manual_bb_policy(self, carbs, glucose, log=False):
+        if carbs > 0:
+            carb_correct = carbs / self.cr
+            hyper_correct = (glucose > self.target) * (glucose - self.target) / self.cf
+            hypo_correct = (glucose < self.low_lim) * (self.low_lim - glucose) / self.cf
+            bolus = 0
+            if self.use_low_lim:
+                bolus -= hypo_correct
+            if self.use_cf:
+                if self.last_cf > self.cooldown and hyper_correct > 0:
+                    bolus += hyper_correct
+                    self.last_cf = 0
+            if self.use_bol:
+                bolus += carb_correct
+            bolus = bolus / self.sample_rate
+        else:
+            bolus = 0
+            carb_correct = 0
+            hyper_correct = 0
+            hypo_correct = 0
+        self.last_cf += self.sample_rate
+        if log:
+            return Action(basal=self.basal, bolus=bolus), hyper_correct, hypo_correct, carb_correct
+        else:
+            return Action(basal=self.basal, bolus=bolus)
+
+    def get_params(self):
+        return ParamTup(basal=self.basal, cf=self.cf, cr=self.cr)
+
+    def adjust(self, basal_adj, cr_adj):
+        self.basal += self.orig_basal + basal_adj
+        self.cr = self.orig_cr * cr_adj
+
+    def reset(self):
+        self.cr = self.orig_cr
+        self.cf = self.orig_cf
+        self.basal = self.orig_basal
+        self.last_cf = np.inf
