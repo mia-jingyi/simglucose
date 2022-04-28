fba65996c7be3f3bc39fd56f86f4d3b92b984c1a
Jingyi Yu <mia@144-152.eduroam.rwth-aachen.de>
2022-04-28 14:55:29 +0200

    Change magni risk reward function

--- rl/reward_functions.py
+++ rl/reward_functions.py
@@ -1,6 +1,12 @@
+import numpy as np
+
+
 def reward_risk(risk_hist, **kwargs):
     return -risk_hist[-1]
 
 
-def reward_magni_risk(magni_risk_hist, **kwargs):
-    return -magni_risk_hist[-1]
+def magni_reward(bg_hist, **kwargs):
+    bg = max(1, bg_hist[-1])
+    fBG = 3.5506*(np.log(bg)**.8353-3.7932)
+    risk = 10 * (fBG)**2
+    return -1*risk
